39be778208a41e5bed52089943f65e7f22a6e2d1
Seven <carlos.ara777@gmail.com>
2022-03-30 00:03:32 -0600

    Segundo Router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,22 @@
-import { Routes, Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import { DCScreen} from '../components/dc/DcScreen';
-import { LoginScreen} from '../components/login/LoginScreen';
-import { MarvelScreen} from '../components/marvel/MarvelScreen';
-import { SearchScreen} from '../components/search/SearchScreen';
+import { LoginScreen } from '../components/login/LoginScreen';
 
+import { DashboardRoutes } from './DashboardRoutes';
 
-import {Navbar} from "../components/ui/Navbar"
 
 
 export const AppRouter = () => {
-    return(
+    return (
         <BrowserRouter>
-            <Navbar/>
+            
             <Routes>
-                <Route path="/" element={<MarvelScreen />} />
-                <Route path="/marvel" element={<MarvelScreen />} />
-                <Route path="/dc" element={<DCScreen />} />
-                <Route path="/search" element={<SearchScreen />} />
+                
                 <Route path="/login" element={<LoginScreen />} />
-      </Routes>
+
+                <Route path="/*" element={ <DashboardRoutes />  } />
+
+            </Routes>
         </BrowserRouter>
     )
 }
